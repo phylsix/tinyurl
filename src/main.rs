38af493fa12325f4a1152d38c18c935b95472b80
Weinan Si <siweinan@gmail.com>
2024-06-04 22:26:27 -0700

    feature: minimum server set up

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+const LISTENER_ADDR: &str = "127.0.0.1:9876";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let listener = TcpListener::bind(LISTENER_ADDR).await?;
+    info!("Listening on: {}", LISTENER_ADDR);
+
+    let app = Router::new().route("/", get(handler));
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
