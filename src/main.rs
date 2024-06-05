b98ea5ab7d9681e810b2426164a3239b46166f5f
Weinan Si <siweinan@gmail.com>
2024-06-04 23:54:20 -0700

    feature: add shorten route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use nanoid::nanoid;
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 use tokio::net::TcpListener;
-use tracing::{info, level_filters::LevelFilter};
+use tracing::{error, info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 const LISTENER_ADDR: &str = "127.0.0.1:9876";
-const DB_ADDR: &str = "postgres://postgres:postgres@localhost:5432/tinyurl";
+const DB_ADDR: &str = "postgres://postgres:postgres@127.0.0.1:5432/tinyurl";
+
+#[derive(Debug, Deserialize)]
+struct ShortenRequest {
+    url: String,
+}
+
+#[derive(Debug, Serialize)]
+struct ShortenResponse {
+    url: String,
+}
 
 #[derive(Debug, Clone)]
 struct AppState {
@@ -23,7 +35,7 @@ async fn main() -> Result<()> {
 
     let state = AppState::try_new().await?;
 
-    let app = Router::new().route("/", get(handler)).with_state(state);
+    let app = Router::new().route("/", post(shorten)).with_state(state);
 
     axum::serve(listener, app.into_make_service()).await?;
 
@@ -49,8 +61,38 @@ impl AppState {
 
         Ok(Self { db })
     }
+
+    async fn shorten(&self, url: &str) -> Result<String> {
+        let id = nanoid!(6);
+
+        sqlx::query(
+            r#"
+            INSERT INTO urls (id, url) VALUES ($1, $2)
+            ON CONFLICT (url) DO NOTHING
+            RETURNING id
+            "#,
+        )
+        .bind(&id)
+        .bind(url)
+        .fetch_one(&self.db)
+        .await?;
+
+        Ok(id)
+    }
 }
 
-async fn handler() -> &'static str {
-    "Hello, World!"
+async fn shorten(
+    State(state): State<AppState>,
+    Json(data): Json<ShortenRequest>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let id = state.shorten(&data.url).await.map_err(|e| {
+        error!("Failed to shorten URL: {:?}", e);
+        StatusCode::UNPROCESSABLE_ENTITY
+    })?;
+
+    let body = Json(ShortenResponse {
+        url: format!("{}/{}", LISTENER_ADDR, id),
+    });
+
+    Ok((StatusCode::CREATED, body))
 }
